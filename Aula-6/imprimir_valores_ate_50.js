9d1123f7e4a89322edb3f56e22abf5bc0a6ad35c
lsferrarez <ferrarezleandro@gmail.com>
2024-04-29 19:36:32 -0300

    feat: criar_uma_lista_de_usuarios_implementada.js

--- Aula-6/imprimir_valores_ate_50.js
+++ Aula-6/imprimir_valores_ate_50.js
@@ -2,7 +2,25 @@
 // 2 - O For Loop é um laço de repetição.
 // 3 - Ele deve ter um "contador".
 // 4 - A cada "iteração" ele deve incrementar o contador caso seja correta a condição.
-for (contador = 0; contador <= 50; contador++) {
-    console.log(contador);
-  }
+// for (contador = 0; contador <= 50; contador++) {
+//     console.log(contador);
+
+// 1 - Necessito criar um lista de usuários com os nomes
+// Sofia
+// Lucas
+// Isabella
+// Mateus
+// Alice
+// 
+// Essa lista deve ser armazenada em uma constante.
+// Quero que na tela seja impresso na saída os nomes, Sofia, Isabella e Alice.
+// Dica: Será necessário utilizar um recurso chamado ARRAY
+//   }
+
+let listaNomes = [ "Sofia","Lucas","Isabella","Mateus","Alice"]
+listaNomes.slice();
+console.log(listaNomes);
+console.log(listaNomes[0])
+console.log(listaNomes[2])
+console.log(listaNomes[4])
   
